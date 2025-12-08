5ecd2fb1f78a75b33339097b3a3391fb25ae3606
Subrat <subrat.bhol@smaketsolutions.com>
2025-12-08 10:51:13 +0530

    Build issue fix

--- src/Components/Auth/Login.tsx
+++ src/Components/Auth/Login.tsx
@@ -1,6 +1,6 @@
 
 import { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import {  useNavigate } from "react-router-dom";
 import toast from "react-hot-toast";
 import api from "../../services/api";
 import logo from "../../assets/utkal.png";
