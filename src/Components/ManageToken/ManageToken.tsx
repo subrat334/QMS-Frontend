5ea95b16bbe39d4499fbcebcb36095a92908ad50
amansmaket <aman.smaket@gmail.com>
2026-01-16 10:54:03 +0530

    added the signalR

--- src/Components/ManageToken/ManageToken.tsx
+++ src/Components/ManageToken/ManageToken.tsx
@@ -292,7 +292,7 @@ useEffect(() => {
   }
 
   console.log("[SIGNALR] Creating hub connection...");
- const CURRENT_SIGNALR_URL = SIGNALR_URLS.LOCAL;   // or SIGNALR_URLS.LIVE
+ const CURRENT_SIGNALR_URL = SIGNALR_URLS.DEV;   // or SIGNALR_URLS.LIVE
 
   const connection = ($ as any).hubConnection(
   CURRENT_SIGNALR_URL,
